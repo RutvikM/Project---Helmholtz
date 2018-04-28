5d3f578d5d5d5e4145daac9dbce7a813c30118d6
Rutvik Mehta <rutvik.mehta08@gmail.com>
2018-04-27 23:54:38 -0500

    Commit 2
    In this commit, I added the arrays for x and y axis and then substituted the values for the grids using the for loop.

--- ComputingProject-Helmholtz.m
+++ ComputingProject-Helmholtz.m
@@ -6,3 +6,11 @@ By = pi;
 pt = 10; % The number of points, NOT the number of discretizations.
 dx = (Bx-Ax)/pt;
 dy = (By-Ay)/pt;
+x = zeros(1,pt);
+y = zeros(1,pt);
+x(1,1) = Ax;
+y(1,1) = Ay;
+for a = 2:pt
+    x(1,a) = Ax+(a*dx); % Grid being made in the for loop.
+    y(1,a) = Ay+(a*dx);
+end
